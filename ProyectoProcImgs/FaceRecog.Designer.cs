1ffbb7476f7454a33117a6a8acc0da4d74016ac2
rickylolo <ricky_lolo29@hotmail.com>
2023-03-22 15:15:18 -0600

    CamaraUpdate

--- ProyectoProcImgs/FaceRecog.Designer.cs
+++ ProyectoProcImgs/FaceRecog.Designer.cs
@@ -29,48 +29,76 @@ namespace ProyectoProcImgs
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.camera_pb = new System.Windows.Forms.PictureBox();
+            this.devicesCB = new System.Windows.Forms.ComboBox();
+            this.chooseWB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.camera_pb)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(41, 40);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(646, 550);
-            this.pictureBox1.TabIndex = 19;
-            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(249, 9);
+            this.label1.Location = new System.Drawing.Point(593, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(206, 28);
             this.label1.TabIndex = 16;
             this.label1.Text = "Reconocimiento Facial";
             // 
+            // camera_pb
+            // 
+            this.camera_pb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.camera_pb.Location = new System.Drawing.Point(343, 64);
+            this.camera_pb.Name = "camera_pb";
+            this.camera_pb.Size = new System.Drawing.Size(703, 551);
+            this.camera_pb.TabIndex = 17;
+            this.camera_pb.TabStop = false;
+            // 
+            // devicesCB
+            // 
+            this.devicesCB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.devicesCB.FormattingEnabled = true;
+            this.devicesCB.Location = new System.Drawing.Point(1052, 643);
+            this.devicesCB.Name = "devicesCB";
+            this.devicesCB.Size = new System.Drawing.Size(279, 23);
+            this.devicesCB.TabIndex = 18;
+            // 
+            // chooseWB
+            // 
+            this.chooseWB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.chooseWB.Location = new System.Drawing.Point(971, 642);
+            this.chooseWB.Name = "chooseWB";
+            this.chooseWB.Size = new System.Drawing.Size(75, 23);
+            this.chooseWB.TabIndex = 19;
+            this.chooseWB.Text = "OK";
+            this.chooseWB.UseVisualStyleBackColor = true;
+            this.chooseWB.Click += new System.EventHandler(this.chooseWB_Click);
+            // 
             // FaceRecog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(736, 656);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1424, 720);
+            this.Controls.Add(this.chooseWB);
+            this.Controls.Add(this.devicesCB);
+            this.Controls.Add(this.camera_pb);
             this.Controls.Add(this.label1);
             this.Name = "FaceRecog";
             this.Text = "FaceRecog";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FaceRecog_FormClosed);
             this.Load += new System.EventHandler(this.FaceRecog_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.camera_pb)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox camera_pb;
+        private System.Windows.Forms.ComboBox devicesCB;
+        private System.Windows.Forms.Button chooseWB;
     }
 }
--- ProyectoProcImgs/FaceRecog.cs
+++ ProyectoProcImgs/FaceRecog.cs
@@ -5,11 +5,17 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
 
 namespace ProyectoProcImgs
 {
     public partial class FaceRecog : Form
     {
+        private bool availableDevices;
+        private FilterInfoCollection myDevices;
+        private VideoCaptureDevice myWebCam;
+
         public FaceRecog()
         {
             InitializeComponent();
@@ -32,6 +38,53 @@ namespace ProyectoProcImgs
         private void FaceRecog_Load(object sender, EventArgs e)
         {
             LoadTheme();
+            LoadDevices();
+        }
+
+        public void LoadDevices()
+        {
+            myDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if(myDevices.Count > 0)
+            {
+                availableDevices = true;
+                for (int i= 0; i < myDevices.Count; i++)           
+                    devicesCB.Items.Add(myDevices[i].Name.ToString());
+                devicesCB.Text = myDevices[0].ToString();
+            }
+            else
+            {
+                availableDevices = false;
+            }
+        }
+
+        public void closeWebCam()
+        {
+            if(myWebCam!=null && myWebCam.IsRunning)
+            {
+                myWebCam.SignalToStop();
+                myWebCam = null;
+            }
+        }
+
+        private void chooseWB_Click(object sender, EventArgs e)
+        {
+            closeWebCam();
+            int i = devicesCB.SelectedIndex;
+            string videoName = myDevices[i].MonikerString;
+            myWebCam = new VideoCaptureDevice(videoName);
+            myWebCam.NewFrame += new NewFrameEventHandler(Capturing);
+            myWebCam.Start();
+        }
+
+        private void Capturing(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            camera_pb.Image = image;
+        }
+
+        private void FaceRecog_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            closeWebCam();
         }
     }
 }
